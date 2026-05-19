ca5d78ea6f5dda2cd287bb86362f9bd16129f87e
edieakepe <124752058+edieakepe@users.noreply.github.com>
2026-05-19 03:49:28 -0700

    Update db.php

--- waste_api/db.php
+++ waste_api/db.php
@@ -1,12 +1,15 @@
 <?php
-$host = "sql300.infinityfree.com";
-$user = "if0_41863641";
-$password = "Akdiamond123";
-$database = "if0_41863641_waste_report_db";
 
-$conn = new mysqli($host, $user, $password, $database);
+$host = "mysql.railway.internal";
+$user = "root";
+$password = "ujnGbbeqhPLavFRtaSKxcjPVqMBmSSEM";
+$database = "railway";
+$port = "3306";
+
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
